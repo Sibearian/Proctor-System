b8b6ab2a77b42af25bbcb0d6d0b2917301214f9b
srujan <srujansarode@protonmail.com>
2021-07-30 18:32:16 +0530

    Made a change to Home
    To make it more consistant with the image in the design
    
    -Srujan

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,41 +1,39 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { Col, Grid, Row } from "rsuite";
+import { Container, Content, Sidebar } from "rsuite";
 
 import Navbar from "../../components/Navbar";
 import Dashboard from "../../components/Dashboard";
-import Messages from "./Messages";
+import Messages from "../../components/Messages";
 import Settings from "./Settings";
 import UserProfile from "./UserProfile";
 
 const Home = () => {
 	return (
-		<Grid>
-			<Row>
-				<Col>
-					<Navbar />
-				</Col>
-				<Col>
-					<Switch>
-						<Route path="/dashboard" exact>
-							<Dashboard />
-						</Route>
-						<Route path="/messages" exact>
-							<Messages />
-						</Route>
-						<Route path="/settings" exact>
-							<Settings />
-						</Route>
-						<Route path="/profile" exact>
-							<UserProfile />
-						</Route>
-						<Route path="/" exact>
-							<Dashboard />
-						</Route>
-					</Switch>
-				</Col>
-			</Row>
-		</Grid>
+		<Container>
+			<Sidebar width={60}>
+				<Navbar />
+			</Sidebar>
+			<Content>
+				<Switch>
+					<Route path="/dashboard" exact>
+						<Dashboard />
+					</Route>
+					<Route path="/messages" exact>
+						<Messages />
+					</Route>
+					<Route path="/settings" exact>
+						<Settings />
+					</Route>
+					<Route path="/profile" exact>
+						<UserProfile />
+					</Route>
+					<Route path="/" exact>
+						<Dashboard />
+					</Route>
+				</Switch>
+			</Content>
+		</Container>
 	);
 };
 
