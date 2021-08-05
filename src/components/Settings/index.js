07e34c88b02fe26ca575856bf9ac1b189bf2c8ef
srujan <srujansarode@protonmail.com>
2021-08-05 13:25:50 +0530

    Stared working on the Settings page
    Doing it's layout currently
    
    -Srujan

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,34 +1,30 @@
 import React from "react";
 import { Avatar, Button, Col, Grid, Row } from "rsuite";
 import { useProfile } from "../../context/profile.context";
-import { useModalState } from "../../misc/custom-hooks";
 
-const acceptedFileTypes = ["image/png", "image/jpeg", "image/pjpeg"];
-const isValidFile = (file) => acceptedFileTypes.includes(file.type);
+// const acceptedFileTypes = ["image/png", "image/jpeg", "image/pjpeg"];
+// const isValidFile = (file) => acceptedFileTypes.includes(file.type);
 
-const getBlob = (canvas) => {
-	return new Promise((resolve, reject) => {
-		canvas.toBlob((blob) => {
-			if (blob) {
-				resolve(blob);
-			} else {
-				reject(new Error("File process error"));
-			}
-		});
-	});
-};
+// const getBlob = (canvas) => {
+// 	return new Promise((resolve, reject) => {
+// 		canvas.toBlob((blob) => {
+// 			if (blob) {
+// 				resolve(blob);
+// 			} else {
+// 				reject(new Error("File process error"));
+// 			}
+// 		});
+// 	});
+// };
 
 const Settings = () => {
 	const { profile } = useProfile();
-	const { isOpen, open, close } = useModalState();
 
-	const onClick = (value) => {
-		console.log(value);
-	};
+	const onClick = () => {};
 
 	return (
 		<div>
-			<Grid>
+			<Grid fluid>
 				<Row>
 					<Col
 						xs={4}
@@ -38,19 +34,47 @@ const Settings = () => {
 						block
 						ripple={false}
 					>
-						<Avatar src={`${profile.avatar}`} size="lg" circle>
-							{String(profile.name)
-								.split(" ")
-								.map((word, index) => {
-									if (index < 2) {
-										return word[0];
-									}
-									return "";
-								})}
-						</Avatar>
-						<Button block size="xs" color="cyan">
-							Change Profile Picture
-						</Button>
+						<Row>
+							<Avatar src={`${profile.avatar}`} size="lg" circle>
+								{String(profile.name)
+									.split(" ")
+									.map((word, index) => {
+										if (index < 2) {
+											return word[0];
+										}
+										return "";
+									})}
+							</Avatar>
+						</Row>
+						<Row>Change Profile Picture</Row>
+					</Col>
+					<Col>
+						<Row>
+							<Col xs={8}>
+								Name : <h6> {profile.name} </h6>
+							</Col>
+						</Row>
+					</Col>
+					<Col>
+						<Row>
+							<Col xs={8}>
+								Register number : <h6> {profile.reg_no} </h6>
+							</Col>
+						</Row>
+					</Col>
+					<Col>
+						<Row>
+							<Col xs={8}>
+								Roll number : <h6> {profile.roll_no} </h6>
+							</Col>
+						</Row>
+					</Col>
+					<Col>
+						<Row>
+							<Col xs={8}>
+								Branch : <h6> {profile.branch} </h6>
+							</Col>
+						</Row>
 					</Col>
 				</Row>
 			</Grid>
